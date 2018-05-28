f8468085ae13e10b48e80ad3b3d7fb3160a7b37e
Führer Taco <geniusnolan@hotmail.com>
2018-05-28 06:28:03 -0400

    Finished the program.

--- u5MathTutorNolan/Program.cs
+++ u5MathTutorNolan/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/**
+ * Written by Nolan Meehan 
+ * Asks the user for the answer of a simple math problem rounds to the nearest tenth.
+ * 
+ * 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +16,32 @@ namespace u5MathTutorNolan
     {
         static void Main(string[] args)
         {
+            Random r = new Random();
+            char[] operators = { '*', '+', '-', '/' };
+            int num1 = r.Next(1, 11);
+            int num2 = r.Next(1, 11);
+            double answer = 0;
+            char oper = '/';//operators[r.Next(operators.Length)];
+            switch (oper)
+            {
+                case '*':
+                    answer = num1 * num2;
+                    break;
+                case '/':
+                    answer = (double)num1 / num2;
+                    break;
+                case '-':
+                    answer = num1 - num2;
+                    break;
+                case '+':
+                    answer = num2 + num1;
+                    break;
+            }
+            answer = Math.Round(answer, 1);
+            Console.WriteLine(String.Format("What's {0} {1} {2}?",num1,oper,num2));
+            if (Math.Round(double.Parse(Console.ReadLine()), 1) == answer) Console.WriteLine("Correct!");
+            else Console.WriteLine("Nope! Answer is " + answer);
+            Console.ReadLine();
         }
     }
 }
